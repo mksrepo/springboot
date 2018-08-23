0ab322b5c739217a19a38780b90a1e90abb2eb38
Mrinmay <mrinmay.santra@gmail.com>
2018-08-23 18:28:08 +0530

    Initial committed

---
+++ cfui-deploy-service/src/main/java/com/cli/cfuideployservice/CfuiDeployServiceApplication.java
@@ -0,0 +1,12 @@
+package com.cli.cfuideployservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfuiDeployServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfuiDeployServiceApplication.class, args);
+	}
+}

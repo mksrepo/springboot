8857ec73113213f36dbeeb5e480d893092c96aa2
Mrinmay <mrinmay.santra@gmail.com>
2018-08-27 12:14:02 +0530

    Committed initial version

---
+++ ipm-boot-rest-service/src/main/java/com/ipm/ipmbootrestservice/IpmBootRestServiceApplication.java
@@ -0,0 +1,12 @@
+package com.ipm.ipmbootrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpmBootRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpmBootRestServiceApplication.class, args);
+	}
+}
---
+++ ipm-boot-rest-service/src/test/java/com/ipm/ipmbootrestservice/IpmBootRestServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ipm.ipmbootrestservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IpmBootRestServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

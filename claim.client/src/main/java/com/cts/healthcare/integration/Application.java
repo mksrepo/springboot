db33ea68a0f942072e4afd8e8a4629c6d620bc17
Mrinmay <mrinmay.santra@gmail.com>
2018-08-28 14:21:15 +0530

    Updated package structure

--- claim.client/src/main/java/com/cts/healthcare/integration/Application.java
+++ claim.client/src/main/java/com/cts/healthcare/integration/Application.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.cts.healthcare.integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- claim.client/src/test/java/com/cts/healthcare/integration/ApplicationTests.java
+++ claim.client/src/test/java/com/cts/healthcare/integration/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.cts.healthcare.integration;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;

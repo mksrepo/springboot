cadbc0bf00b83e9a1ecae068677fa14b2abb4f7c
Mrinmay Santra <mrinmay.santra@gmail.com>
2018-08-31 00:19:16 +0530

    committed initial version

---
+++ spring-ext-config-client/src/main/java/com/ext/config/SpringExtConfigClientApplication.java
@@ -0,0 +1,29 @@
+package com.ext.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringExtConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExtConfigClientApplication.class, args);
+	}
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+ 
+    @Value("${msg:Hello world - Config Server is not working..pelase check}")
+    private String msg;
+ 
+    @RequestMapping("/msg")
+    String getMsg() {
+        return this.msg;
+    }
+}

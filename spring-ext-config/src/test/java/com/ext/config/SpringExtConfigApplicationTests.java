7130ab7d089cb5579c71da6011a1d84869ae1656
Mrinmay Santra <mrinmay.santra@gmail.com>
2018-08-30 22:57:56 +0530

    Committed initial version

---
+++ spring-ext-config/src/test/java/com/ext/config/SpringExtConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ext.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringExtConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

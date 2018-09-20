ffab3cc9c80878560615a4081d03fe00128d1a1d
264030 <264030@PC399852.cts.com>
2018-09-20 12:36:16 +0530

    Committed the initial version.

--- directory-service/src/main/java/io/pivotal/repository/DirectoryRepository.java
+++
@@ -1,9 +0,0 @@
-package io.pivotal.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import io.pivotal.domain.Directory;
-
-public interface DirectoryRepository extends CrudRepository<Directory, Long> {
-
-}
---
+++ oauth2-jwt-security/src/main/java/com/security/Oauth2JwtSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2JwtSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2JwtSecurityApplication.class, args);
+	}
+}

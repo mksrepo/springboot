73b68c8d8db20409db89828dbffde3e544ddf472
Mrinmay <mrinmay.santra@gmail.com>
2018-10-31 10:36:06 +0530

    Initial commit

---
+++ emp-perf-service/src/main/java/com/emp/empperfservice/EmpPerfServiceApplication.java
@@ -0,0 +1,12 @@
+package com.emp.empperfservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpPerfServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpPerfServiceApplication.class, args);
+	}
+}
---
+++ emp-perf-service/src/test/java/com/emp/empperfservice/EmpPerfServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.emp.empperfservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmpPerfServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

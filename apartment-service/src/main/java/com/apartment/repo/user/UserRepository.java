083dcc39026754a840d977946881387814470eb3
mrinmay <mrinmay.santra@gmail.com>
2019-01-04 12:17:33 +0530

    Initial commit

--- apartment-service/src/main/java/com/apartment/repo/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.apartment.repo.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.apartment.entity.user.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
--- apartment-service/src/main/resources/schema.sql
+++
@@ -1,1 +0,0 @@
-mysql -u root -p password1$ -e "create database APARTMENT_DB"

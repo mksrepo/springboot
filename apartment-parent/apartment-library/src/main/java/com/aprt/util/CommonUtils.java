f3a1027c7238e94a5923a70ef00884f83e70d94f
mrinmay <mrinmay.santra@gmail.com>
2019-01-04 17:06:53 +0530

    Test-Case implementation

--- apartment-parent/apartment-library/src/main/java/com/aprt/util/CommonUtils.java
+++ apartment-parent/apartment-library/src/main/java/com/aprt/util/CommonUtils.java
@@ -1,7 +1,40 @@
+/*
+ * CommonUtils.java
+ * Copyright (c) 2019 Binary Total Solutions
+ * 
+ * Description: common utility provider
+ * 
+ * Maintenance History
+ * YYMMDD	Who					Reason
+ * ==================================================================
+ * 190104	Mrinmay Santra		Initial Version
+ */
 package com.aprt.util;
 
+/**
+ * <h1>Utility Provider</h1>
+ * This class is responsible for providing common utilities
+ * 
+ * <p>
+ * <b>Note:</b> This class doesn't carries out any requirement
+ *
+ * @author Mrinmay Santra
+ * @version 1.0
+ * @since 2019-01-34
+ */
 public class CommonUtils {
+	
+	/**
+	 * This method is responsible to check null of any given object
+	 * 
+	 * @author Mrinmay Santra
+	 * @param object
+	 * @return nothing
+	 * @exception nothing
+	 */
 	public static boolean isEmptyObject(Object object) {
+		
+		// Null checking and returning result 
 		return object == null;
 	}
 }
